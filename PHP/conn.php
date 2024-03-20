814f15826d47915852bc977ba958ea578a659300
Refilx1 <147277909+Refilx1@users.noreply.github.com>
2024-03-20 14:35:24 -0300

    Ricardo Amaro
    1 - Criado o arquivo cadastro de ingredientes
    
    2- Primeira parte da conexão com banco de dados feita

--- PHP/conn.php
+++ PHP/conn.php
@@ -1,12 +1,12 @@
 <?php
   $username = "root";
   $password = "";
-  $database = "pitissariadb";
-  $server = "127.0.0.1";
+  $database = "pitisaria";
+  $server = "127.0.0.1:3306";
 
   $conn = new mysqli_connect($server, $username, $password, $database);
   
   if($conn->connect_error){
-    die("Não foi possivel conectar: " . $conn->connect_error)
-    }
+    die("Não foi possível conectar: " . $conn->connect_error);
+  }
 ?>
