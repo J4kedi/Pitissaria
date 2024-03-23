20957200132acc1621e910ad5ab8ed6983673b2f
Refilx1 <147277909+Refilx1@users.noreply.github.com>
2024-03-23 12:15:07 -0300

    Ricardo
    Pagina cadastro de ingredientes feita

--- php/cadastro_ingredientes.php
+++ php/cadastro_ingredientes.php
@@ -1,13 +1,23 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Cadastro de Ingredientes</title>
 </head>
 <body>
-    <h1>
-        ola mundo testando
-    </h1>
+    <h2>Cadastro de Ingredientes</h2>
+    <form action="cadastro_ingredientes_php.php" id="form1" method="POST">
+        <label for="nome">Nome do Ingrediente:</label><br>
+        <input type="text" id="nome" name="nome" required><br><br>
+
+        <label for="validade">Data de Validade:</label><br>
+        <input type="date" id="validade" name="validade" required><br><br>
+
+        <label for="quantidade">Quantidade:</label><br><br>
+        <input type="number" id="quantidade" name="quantidade" required><br><br>
+
+        <input type="submit" value="Cadastrar Ingrediente">
+    </form>
 </body>
 </html>
