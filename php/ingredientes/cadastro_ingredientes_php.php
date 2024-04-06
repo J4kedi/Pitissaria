d0c2e15d975cc1ec747075a73907205c139c341b
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-04-06 13:16:01 -0300

    Alterado para echo
    Foi alterado os alertas da pagina de cadastro de ingredientes de alert para echo

--- php/ingredientes/cadastro_ingredientes_php.php
+++ php/ingredientes/cadastro_ingredientes_php.php
@@ -1,36 +1,30 @@
 <?php
-    include("connection.php");
+include("connection.php");
 
-    $nome = $_POST["nome_ingrediente"];
-    $validade = $_POST["dt_validade"];
-    $quantidade = $_POST["quantidade_ingrediente"];
-    $preco_compra = $_POST["preco_compra"];
+$nome = $_POST["nome_ingrediente"];
+$validade = $_POST["dt_validade"];
+$quantidade = $_POST["quantidade_ingrediente"];
+$preco_compra = $_POST["preco_compra"];
 
-    $sql = "SELECT * FROM ingredientes WHERE nome_ingrediente = '$nome'";
-    $result = $conn->query($sql);
+$sql = "SELECT * FROM ingredientes WHERE nome_ingrediente = '$nome'";
+$result = $conn->query($sql);
 
-    if ($result->num_rows > 0) {
-        // O produto já existe, exibir uma mensagem de erro
-        ?>
-        <script>
-            window.alert("Produto já cadastrado!!!");
-            window.location.href = "lista_ingredientes.php";
-        </script>
-        <?php
+if ($result->num_rows > 0) {
+    // O ingrediente já existe, exibir uma mensagem de erro em outra pagina
+    echo '<h1>Ingrediente já cadastrado. Por favor, verifique novamente.</h1>'; //aqui ele mostra a pagina com o texto em H1
+    echo '<script>setTimeout(function() { history.back(); }, 3000);</script>'; // Atraso de 3 segundos 
+} else {
+    // O ingrediente não existe, inserir no banco de dados 
+    $sql_insert = "INSERT INTO ingredientes(nome_ingrediente, dt_validade, quantidade_ingrediente, preco_compra) VALUES('$nome', '$validade','$quantidade','$preco_compra')";
+    
+    if ($conn->query($sql_insert) === TRUE) {
+        echo "<p>Ingrediente cadastrado com sucesso.</p>";
+        echo '<script>setTimeout(function() { history.back(); }, 3000);</script>'; // Atraso de 3 segundos 
     } else {
-        // O produto não existe, inserir no banco de dados
-        $sql_insert = "INSERT INTO ingredientes(nome_ingrediente, dt_validade, quantidade_ingrediente, preco_compra) VALUES('$nome', '$validade','$quantidade','$preco_compra')";
-        
-        if ($conn->query($sql_insert) === TRUE) {
-        ?>
-            <script>
-            window.alert("Produto cadastrado com sucesso!");
-            window.location.href = "lista_ingredientes.php";
-        </script>
-        <?php
-            
-        } else {
-            echo "Erro ao cadastrar o produto: " . $conn->error;
-        }
+        echo "Erro ao cadastrar o ingrediente: " . $conn->error;
     }
+}
+
+// Feche a conexão após o uso, se necessário
+$conn->close();
 ?>
