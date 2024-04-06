558a0cc8357c85d5c15057955202331d28a8fbb5
ArthurHermes <arthurfhermes@gmail.com>
2024-04-06 12:43:43 -0300

    Add Icon em todas as paginas

--- php/ingredientes/edit_ingredientes.php
+++ php/ingredientes/edit_ingredientes.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Ingredientes</title>
     <link rel="stylesheet" href="../../Style/edit_ingredientes.css">
+    <link rel="shortcut icon" href="../../imagens/icone/pizza.ico" type="image/x-icon">
 </head>
 <body>
 <?php
--- php/ingredientes/ingredientes_ingrediente.php
+++ php/ingredientes/ingredientes_ingrediente.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Detalhes do Ingrediente</title>
     <link rel="stylesheet" href="../../Style/ingredientes_ingredientes.css">
+    <link rel="shortcut icon" href="../../imagens/icone/pizza.ico" type="image/x-icon">
 </head>
 <body>
     <?php
