67e2c6e9045d906a490d622755e91113a31611b3
Kauan Pardini <kauan.pardini@pucpr.edu.br>
2024-04-09 10:08:10 -0300

    Colocado com pastas separadas, o header e footer para padronização do site, criado página de estilo padrao para estas duas páginas e todas as páginas HTML como php

---
+++ geral/footer.php
@@ -0,0 +1,34 @@
+<footer class="text-center bg-dark">
+    <section class="pb-4">
+      <h2 class="text-center">Facilidades que oferecemos</h2>
+    
+      <div class="d-flex flex-column align-items-center gap-3">
+        <div class="section-facilidades d-flex">
+          <div><i class="bi bi-cash-coin fs-1"></i></div>
+          <div>
+            <div class="texto-section-maior ms-3">PAGAMENTO FACILITADO</div>
+            <div class="texto-section-menor ms-3">Aceitamos todos os cartões, pix e dinheiro.</div>
+          </div>
+        </div>
+    
+        <div class="section-facilidades d-flex">
+          <div><i class="bi bi-truck fs-1"></i></div>
+          <div>
+            <div class="texto-section-maior ms-3">ONDE VOCÊ ESTIVER</div>
+            <div class="texto-section-menor ms-3">Graças a nossa exclusividade como delivery,
+              você pode escolher pela forma de entrega ou retirada no local.</div>
+          </div>
+        </div>
+    
+        <div class="section-facilidades d-flex">
+          <div><i class="bi bi-egg-fried fs-1"></i></div>
+          <div>
+            <div class="texto-section-maior ms-3">MONTE SUA PIZZA</div>
+            <div class="texto-section-menor ms-3">Nossa equipe de cozinha prepara cada pizza com
+              ingredientes selecionados de acordo com a sua preferência.</div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <p>2024 <i class="bi bi-c-circle"></i> Desenvolvido por Tsurubas</p>
+</footer>
