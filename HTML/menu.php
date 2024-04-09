8c43c82042e2d1dddf71d86152130c2ce3561737
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-04-09 10:22:34 -0300

    Update menu.php

--- HTML/menu.php
+++ HTML/menu.php
@@ -1,5 +1,5 @@
 <?php
-		require('verifica_login.php');
+		require('verifica_login_php.php');
 		if ($_SESSION['tp_user'] == 'gerente'){
 			$url = "Location: /" . $url . "ingredientes/lista_ingredientes.php";	// Monta página para reurlecionamento
 			header($url);                                         		// Vai para a página de login / inicial
