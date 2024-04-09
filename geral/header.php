f23c3a19a10da10fa71c7f61f970e4092c7fd054
Kauan Pardini <kauan.pardini@pucpr.edu.br>
2024-04-09 10:12:58 -0300

    modificado a página login.php para ficar padronizada com as demais e mudado o redirecionado do header para as páginas corretas

--- geral/header.php
+++ geral/header.php
@@ -1,7 +1,7 @@
 <header>
     <nav class="navbar navbar-expand-md navbar-warning">
     <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">
+        <a class="navbar-brand" href="../HTML/index.php">
         <h2 class="m-0"><img class="d-block" src="../ASSETS/logo-inicio (1).png" width="150" alt="logo"></h2>
         </a>
         <button class="navbar-toggler navbar-warning" type="button" data-bs-toggle="collapse"
@@ -12,10 +12,10 @@
         <div class="collapse navbar-collapse navbar-warning" id="navbarSupportedContent">
         <ul class="navbar-nav me-auto navbar-warning">
             <li class="nav-item">
-            <a class="nav-link" href="../HTML/login.html">LOGIN</a>
+            <a class="nav-link" href="../HTML/login.php">LOGIN</a>
             </li>
             <li class="nav-item">
-            <a class="nav-link" href="../HTML/cadastro.html">CADASTRO</a>
+            <a class="nav-link" href="../HTML/cadastro.php">CADASTRO</a>
             </li>
         </ul>
         <form class="d-flex" role="search">
