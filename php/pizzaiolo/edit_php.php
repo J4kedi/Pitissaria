f0625b9cb1f2d856f2ebb75da0dbbe81e608a248
Arthur Hermes <89710458+ArthurHermes@users.noreply.github.com>
2024-04-16 17:26:54 -0300

    Create edit_php

---
+++ php/pizzaiolo/edit_php.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Ingredientes</title>
+    <link rel="stylesheet" href="../../Style/cad_ingredientes.css">
+</head>
+<body>
+<?php
+    include("../connection.php");
+    include("validacao_acesso_php.php");
+    verificar_acesso_gerente();
+    verificar_acesso_pizzaiolo();
+    verificarAcessoGerenteEPizzaiolo();
+    
+    
+    $id = $_POST["id_user"];
+
+    $nome = $_POST["nome"];
+
+    $username = $_POST["username"];
+
+    $cpf = $_POST["cpf"];
+
+    $email = $_POST["email"];
+
+    $senha = $POST["senha"];
+
+    $dt_nasc = $POST["dt_nasc"];
+
+    $num_telefone = $POST["num_telefone"];
+
+    $estado = $POST["estado"];
+
+    $cep = $POST["cep"];
+
+    $cidade = $POST["cidade"];
+
+    $rua = $POST["rua"];
+
+    $sql = "UPDATE user_endereco_user SET nome = '$nome', username = '$username', cpf = '$cpf', email = '$email', senha = '$senha', dt_nasc = '$dt_nasc', num_telefone = '$num_telefone', estado = '$estado',cep = '$cep', cidade = '$cidade', rua = '$rua' WHERE id_user = $id";
+    $result =  $conn->query($sql);
+
+    if ($result === TRUE) {
+        echo "<h1>Dados alterados com sucesso.</h1>";
+        echo '<script>setTimeout(function() { window.location.href = "lista_ingredientes.php"; }, 2000);</script>'; // Redireciona para lista_ingredientes.php após 2 segundos 
+    }   
+    else{
+        echo "Erro ao cadastrar ingrediente: " . $conn->error;
+    }
+?>
+
+</body>
+</html>
+
