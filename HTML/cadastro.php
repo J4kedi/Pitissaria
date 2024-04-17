de1098a57956442f6d3c31dfb33c7a2705dc9c38
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-04-17 18:43:08 -0300

    Atualização das paginas de usuarios
    Foi atualizado

--- HTML/cadastro.php
+++ HTML/cadastro.php
@@ -6,18 +6,18 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.2/assets/css/docs.css" rel="stylesheet">
-    <link rel="stylesheet" href="../../Style/style.css">
-    <link rel="stylesheet" href="../../Style/index.css">
-    <link rel="stylesheet" href="../../Style/padrao.css">
+    <link rel="stylesheet" href="../Style/style.css">
+    <link rel="stylesheet" href="../Style/index.css">
+    <link rel="stylesheet" href="../Style/padrao.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" rel="stylesheet">
     <title>Cadastro</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
-    <link rel="shortcut icon" href="../../imagens/icone/pizza.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../imagens/icone/pizza.ico" type="image/x-icon">
 </head>
 
 <body>
-    <?php include '../../geral/novoheader.php'?>
+    <?php include '../geral/novoheader.php'?>
     
     <main class="container">
         <!-- navegação login e cadastro -->
