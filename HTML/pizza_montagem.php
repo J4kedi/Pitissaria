6de03ca3dd21a54a10eabee0f95259f00d1c1b86
ValArndt <142906934+ValArndt@users.noreply.github.com>
2024-05-03 14:29:26 -0300

    update pizza montagem

---
+++ HTML/pizza_montagem.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="../Style/padrao.css">
+    <title>MOntagem de pizzas</title>
+</head>
+<body>
+    <?php include('../geral/menu.php')?>
+
+    <main class="container">
+        <h1>Monte sua pizza aqui</h1>
+    </main>
+
+    <?php include('../geral/footer.php')?>
+</body>
+</html>
