3c874d91a436b479bf4eb51cd41563cb39f2cb83
ValArndt <142906934+ValArndt@users.noreply.github.com>
2024-05-08 18:23:27 -0300

    valdemar
    mechendo no js da montagem

--- Js/montagem.js
+++ Js/montagem.js
@@ -1,26 +1,26 @@
 document.getElementById('calcular-total').addEventListener('click', function() {
+    var total = 0;
     var tamanho = document.querySelector('input[name="tamanho"]:checked');
     var queijo = document.getElementById('queijo').checked;
     var tomate = document.getElementById('tomate').checked;
 
-    var precoBase = 0;
     if (tamanho) {
         if (tamanho.id === 'pequena') {
-            precoBase += 25;
+            total += 25;
         } else if (tamanho.id === 'media') {
-            precoBase += 35;
+            total += 35;
         } else if (tamanho.id === 'grande') {
-            precoBase += 69;
+            total += 69;
         }
     }
 
     if (queijo) {
-        precoBase += 7;
+        total += 7;
     }
 
     if (tomate) {
-        precoBase += 1;
+        total += 1;
     }
 
-    document.getElementById('total').textContent = 'Total: R$ ' + precoBase.toFixed(2);
+    document.getElementById('total').textContent = 'Total: R$ ' + total.toFixed(2);
 });
