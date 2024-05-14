ccb7a7eba7c81a93c9d7025150ffee732c51efa3
ValArndt <142906934+ValArndt@users.noreply.github.com>
2024-05-14 08:22:29 -0300

    valdemar

--- Js/montagem.js
+++ Js/montagem.js
@@ -1,4 +1,4 @@
-// Função para calcular o total
+// Funcao para calcular o total
 function calcularTotal() {
     var total = 0;
     var tamanho = document.querySelector('input[name="tamanho"]:checked');
@@ -28,7 +28,7 @@ function calcularTotal() {
     document.getElementById('total').textContent = 'Total: R$ ' + total.toFixed(2);
 }
 
-// Adiciona eventos de mudança às checkboxes e campos de quantidade
+// Adiciona eventos de mudanca as checkboxes e campos de quantidade
 document.getElementById('queijo').addEventListener('change', calcularTotal);
 document.getElementById('quantidade_queijo').addEventListener('input', calcularTotal);
 document.getElementById('tomate').addEventListener('change', calcularTotal);
