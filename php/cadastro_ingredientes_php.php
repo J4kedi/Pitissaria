1c9b379ea5e59758160381130a4237c3222648cb
ValArndt <142906934+ValArndt@users.noreply.github.com>
2024-05-14 10:48:21 -0300

    valdemar
    igredientes

--- php/cadastro_ingredientes_php.php
+++ php/cadastro_ingredientes_php.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $preco_compra = $_POST["preco"];
 
     // Preparar e executar a consulta SQL para inserir os dados
-    $sql = "INSERT INTO ingredientes (nome, data_validade, data_entrada, quantidade, preco) VALUES ('$nome', '$quantidade','$validade','$data_entrada','$preco_compra')";
+    $sql = "INSERT INTO ingredientes (nome, data_validade, data_entrada, quantidade, preco) VALUES ('$nome', '$data_validade','$data_entrada','$quantidade','$preco_compra')";
     $stmt = $conn->prepare($sql);
 
     if ($stmt->execute()) {
