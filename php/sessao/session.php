423c8a2c5bf6054f8a59dd60ff0d460e4322ac5d
Ricardo Amaro <147277909+Refilx1@users.noreply.github.com>
2024-06-01 10:53:28 -0300

    Criado sessão de pizzaiolo

--- php/sessao/session.php
+++ php/sessao/session.php
@@ -5,7 +5,6 @@
             <a href = '../paginas/index.php' class='nav-item'>Inicio</a>
             <a href = '../paginas/pizza_montagem.php' class='nav-item'>Personalizado</a>
             <a href = '../paginas/pizzas_prontas.php' class='nav-item'>Cardapio</a>
-            <a href='../PHP/logout.php' class='nav-item'>Logout</a>
 
         ";
 
@@ -27,6 +26,17 @@
             echo "
                 <div class='logar'>
                     <a href = '../HTML/carrinho_teste.php' class='nav-item'>Carrinho</a>
+                    <a href='../paginas/perfil.php' class='nav-item'>" . $_SESSION['username'] . "</a>
+                    <span class='barra'>/</span>
+                    <a href='../PHP/logout.php' class='nav-item'>Logout</a>
+                </div>
+            ";
+        }
+
+        if ($_SESSION['tipo_usuario'] == 'pizzaiolo') {
+            echo "
+                <div class='logar'>
+                    <a href = '../HTML/pedidos_pizzaiolo.php' class='nav-item'>Pedidos</a>
                     <a href='../paginas/perfil.php' class='nav-item'>" . $_SESSION['username'] . "</a>
                     <span class='barra'>/</span>
                     <a href='../PHP/logout.php' class='nav-item'>Logout</a>
